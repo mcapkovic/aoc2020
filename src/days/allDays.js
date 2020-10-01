45e2871d6eae164564b07f741bd39f5a7daee7c4
Matej Capkovic <mcapkovic@gmail.com>
2020-10-01 06:57:25 +0200

    Add advent days

--- src/days/allDays.js
+++ src/days/allDays.js
@@ -1,53 +1,53 @@
 import { solution as day1 } from "./day1/solution";
 import { solution as day2 } from "./day2/solution";
-// import { solution as day3 } from "./day3/solution";
-// import { solution as day4 } from "./day4/solution";
-// import { solution as day5 } from "./day5/solution";
-// import { solution as day6 } from "./day6/solution";
-// import { solution as day7 } from "./day7/solution";
-// import { solution as day8 } from "./day8/solution";
-// import { solution as day9 } from "./day9/solution";
-// import { solution as day10 } from "./day10/solution";
-// import { solution as day11 } from "./day11/solution";
-// import { solution as day12 } from "./day12/solution";
-// import { solution as day13 } from "./day13/solution";
-// import { solution as day14 } from "./day14/solution";
-// import { solution as day15} from './day15/solution';
-// import { solution as day16} from './day16/solution';
-// import { solution as day17} from './day17/solution';
-// import { solution as day18} from './day18/solution';
-// import { solution as day19 } from "./day19/solution";
-// import { solution as day20} from './day20/solution';
-// import { solution as day21} from './day21/solution';
-// import { solution as day22} from './day22/solution';
-// import { solution as day23} from './day23/solution';
-// import { solution as day24} from './day24/solution';
-// import { solution as day25} from './day25/solution';
+import { solution as day3 } from "./day3/solution";
+import { solution as day4 } from "./day4/solution";
+import { solution as day5 } from "./day5/solution";
+import { solution as day6 } from "./day6/solution";
+import { solution as day7 } from "./day7/solution";
+import { solution as day8 } from "./day8/solution";
+import { solution as day9 } from "./day9/solution";
+import { solution as day10 } from "./day10/solution";
+import { solution as day11 } from "./day11/solution";
+import { solution as day12 } from "./day12/solution";
+import { solution as day13 } from "./day13/solution";
+import { solution as day14 } from "./day14/solution";
+import { solution as day15} from './day15/solution';
+import { solution as day16} from './day16/solution';
+import { solution as day17} from './day17/solution';
+import { solution as day18} from './day18/solution';
+import { solution as day19 } from "./day19/solution";
+import { solution as day20} from './day20/solution';
+import { solution as day21} from './day21/solution';
+import { solution as day22} from './day22/solution';
+import { solution as day23} from './day23/solution';
+import { solution as day24} from './day24/solution';
+import { solution as day25} from './day25/solution';
 
 export const allDays = [
   day1,
   day2,
-  // day3,
-  // day4,
-  // day5,
-  // day6,
-  // day7,
-  // day8,
-  // day9,
-  // day10
-  // day11,
-  // day12
-  // day13,
-  // day14,
-  // day15,
-  // day16,
-  // day17,
-  // day18,
-  // day19,
-  // day20,
-  // day21
-  // day22,
-  // day23,
-  // day24,
-  // day25,
+  day3,
+  day4,
+  day5,
+  day6,
+  day7,
+  day8,
+  day9,
+  day10,
+  day11,
+  day12,
+  day13,
+  day14,
+  day15,
+  day16,
+  day17,
+  day18,
+  day19,
+  day20,
+  day21,
+  day22,
+  day23,
+  day24,
+  day25,
 ];
