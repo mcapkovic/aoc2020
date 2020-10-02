d06edbcf8daf7a170e661a5152c25b24157781a9
Matej Capkovic <mcapkovic@gmail.com>
2020-10-02 20:32:59 +0200

    Edit strings

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,25 @@
-export const dayWelcome = (dayNumber) =>
-  `############ Hello, from day ${dayNumber} ############`;
-export const part1Welcome = "-- First part of the solution --";
-export const part2Welcome = "-- Second part of the solution --";
+// export const dayWelcome = (dayNumber) =>
+//   `############ Hello, from day ${dayNumber} ############`;
+
+export const dayWelcome = (dayNumber) => {
+  if (dayNumber > 9)
+    return `######################################
+#------------ Day${dayNumber} -----------------#
+######################################`;
+
+  return `######################################
+#------------- Day${dayNumber} -----------------#
+######################################`;
+};
+
+export const part1Welcome = `#---- First part of the solution ----#
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓`;
+
+export const part2Welcome = `#--- Second part of the solution ----#
+↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓`;
+// export const part2Welcome = "-- Second part of the solution --";
 
 export const noDayFound = "no day found";
-export const loadingFile = "Loading input from local file.";
+export const loadingFile = "Loading input from the local file.";
 export const missingSession = "Missing session. Can not fetch the input.";
 export const inputFetching = "Fetching the input.";
