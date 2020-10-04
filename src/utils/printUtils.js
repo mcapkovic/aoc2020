f71fbefed6862a9c6ffd1b6194147d15fdb668a0
Matej Capkovic <mcapkovic@gmail.com>
2020-10-04 07:38:57 +0200

    Fix input console print

--- src/utils/printUtils.js
+++ src/utils/printUtils.js
@@ -3,13 +3,13 @@ export function printTitle(message){
 }
 
 export function printOriginalInput(message){
-    console.log('\x1b[34m%s\x1b[0m', 'Original input:');
-    console.log('\x1b[34m%s\x1b[0m', message);
+    console.log('\x1b[36m%s\x1b[0m', 'Original input:');
+    console.log('\x1b[36m%s\x1b[0m', message);
 }
 
 export function printParsedInput(message){
-    console.log('\x1b[36m%s\x1b[0m', 'Current input:');
-    console.log('\x1b[36m%s\x1b[0m', message);
+    console.log('Current input:');
+    console.log(message);
 }
 
 export function printAnswer(message){
