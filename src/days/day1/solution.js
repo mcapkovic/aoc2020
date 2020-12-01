c7927b20c1caf4ee174964aa1f5435709cd8fd28
Matej Capkovic <mcapkovic@gmail.com>
2020-12-01 06:38:42 +0100

    Add day 1

--- src/days/day1/solution.js
+++ src/days/day1/solution.js
@@ -1,24 +1,62 @@
 export function partOneCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part1 answer.";
+  let numbers = [];
+  // input.forEach((mainNumber) => {
+  //   input.forEach((subNumber) => {
+  //     if (mainNumber + subNumber === 2020) {
+  //       numbers = [mainNumber, subNumber];
+  //     }
+  //   });
+  // });
+
+  input.some((mainNumber) => {
+    input.some((subNumber) => {
+      if (mainNumber + subNumber === 2020) {
+        numbers = [mainNumber, subNumber];
+        return true;
+      }
+    });
+  });
+
+  const result = numbers[0] * numbers[1];
+
+  return result;
 }
 
 export function partTwoCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part2 answer.";
+  let numbers = [];
+  input.some((mainNumber) => {
+    input.some((subNumber) => {
+      input.some((subSubNumber) => {
+        if (mainNumber + subNumber + subSubNumber === 2020) {
+          numbers = [mainNumber, subNumber, subSubNumber];
+          return true;
+        }
+      });
+    });
+  });
+
+  const result = numbers.reduce(
+    (accumulator, currentValue) => accumulator * currentValue
+  );
+
+  return result;
 }
+
+const p1e1 = `
+1721
+979
+366
+299
+675
+1456
+`;
 
 export function inputParse(originalInput) {
   const currentInput = originalInput;
-  let parsedInput = currentInput;
-   /**
-   * Add input logic here
-   */
-
+  let parsedInput = currentInput
+    .trim()
+    .split("\n")
+    .map((string) => Number(string));
   return {
     inputToPrint: currentInput, // *optional - inputToPrint will be printed if available
     parsedInputToPrint: parsedInput, // *optional - parsedInputToPrint will be printed if available
