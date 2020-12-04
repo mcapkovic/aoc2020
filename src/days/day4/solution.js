889158cd4e8086fb6849a58e38ebd624d9e4dbaf
Matej Capkovic <mcapkovic@gmail.com>
2020-12-04 07:30:29 +0100

    Add day4

--- src/days/day4/solution.js
+++ src/days/day4/solution.js
@@ -1,23 +1,154 @@
 export function partOneCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part1 answer.";
+  const required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+  let validPassports = [];
+  input.forEach((passport) => {
+    let hasAll = true;
+    required.forEach((field) => {
+      if (passport[field] === undefined) hasAll = false;
+    });
+    if (hasAll) validPassports.push(passport);
+  });
+  return validPassports.length;
 }
 
+const validator = {
+  byr: (data) => {
+    if (!data) return false;
+    const value = Number(data);
+    if (value < 1920) return false;
+    if (value > 2002) return false;
+    return true;
+  },
+  iyr: (data) => {
+    if (!data) return false;
+    const value = Number(data);
+    if (value < 2010) return false;
+    if (value > 2020) return false;
+    return true;
+  },
+  eyr: (data) => {
+    if (!data) return false;
+    const value = Number(data);
+    if (value < 2020) return false;
+    if (value > 2030) return false;
+    return true;
+  },
+  hgt: (data) => {
+    if (!data) return false;
+    const height = data.match(/^(([0-9]*)((cm|in)))*$/);
+    if (!height) return false;
+
+    const number = Number(height[2]);
+    if (height[3] === "in") {
+      if (number < 59) return false;
+      if (number > 76) return false;
+    } else {
+      if (number < 150) return false;
+      if (number > 193) return false;
+    }
+    return true;
+  },
+  hcl: (data) => {
+    if (!data) return false;
+    if (!data.match(/^#(([\da-f]{3}){1,2}|([\da-f]{4}){1,2})$/)) return false;
+    return true;
+  },
+  ecl: (data) => {
+    if (!data) return false;
+    const validColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+    if (!validColors.includes(data)) return false;
+    return true;
+  },
+  pid: (data) => {
+    if (!data) return false;
+    if (data.length != 9) return false;
+    if (isNaN(data)) return false;
+    return true;
+  },
+};
+
 export function partTwoCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part2 answer.";
+  const required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+  let validPassports = [];
+  input.forEach((passport) => {
+    let hasAll = true;
+    required.forEach((field) => {
+      if (passport[field] === undefined) hasAll = false;
+      if (!validator[field](passport[field])) {
+        hasAll = false;
+      }
+    });
+    if (hasAll) validPassports.push(passport);
+  });
+  console.log(validPassports);
+  return validPassports.length;
 }
+
+const e1p1 = `
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
+`;
+
+const e1p2 = `
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+`;
+
+const e2p2 = `
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+`;
 
 export function inputParse(originalInput) {
   const currentInput = originalInput;
-  let parsedInput = currentInput;
-   /**
-   * Add input logic here
-   */
+  let parsedInput = [];
+
+  let passport = {};
+
+  currentInput.split("\n").forEach((row) => {
+    if (row === "") {
+      parsedInput.push(passport);
+      passport = {};
+    } else {
+      row.split(" ").forEach((string) => {
+        const pair = string.split(":");
+        passport[pair[0]] = pair[1];
+      });
+    }
+  });
 
   return {
     inputToPrint: currentInput, // *optional - inputToPrint will be printed if available
