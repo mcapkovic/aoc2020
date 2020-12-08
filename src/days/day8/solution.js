ac96dc530be065a39947b96ef71f74166fb81d3d
Matej Capkovic <mcapkovic@gmail.com>
2020-12-08 06:35:31 +0100

    Add d8p1

--- src/days/day8/solution.js
+++ src/days/day8/solution.js
@@ -1,8 +1,34 @@
+const operations = {
+  acc: (argument, position, value) => {
+    return { newValue: argument + value, positionIncrement: position + 1 };
+  },
+  jmp: (argument, position, value) => {
+    return { newValue: value, positionIncrement: argument + position };
+  },
+  nop: (argument, position, value) => {
+    return { newValue: value, positionIncrement: position + 1 };
+  },
+};
+
 export function partOneCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part1 answer.";
+  const usedInstructions = new Set();
+  let position = 0;
+  let value = 0;
+
+  do {
+    if (usedInstructions.has(position)) return value;
+    usedInstructions.add(position);
+
+    const instruction = input[position];
+    const { newValue, positionIncrement } = operations[instruction.operation](
+      instruction.argument,
+      position,
+      value
+    );
+
+    value = newValue;
+    position = positionIncrement;
+  } while (true);
 }
 
 export function partTwoCode(input) {
@@ -12,12 +38,28 @@ export function partTwoCode(input) {
   return "Part2 answer.";
 }
 
+const e1p1 = `
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+`;
+
 export function inputParse(originalInput) {
-  const currentInput = originalInput;
-  let parsedInput = currentInput;
-   /**
-   * Add input logic here
-   */
+  let currentInput = originalInput;
+  // currentInput = e1p1;
+  let parsedInput = currentInput
+    .trim()
+    .split("\n")
+    .map((row) => {
+      const instruction = row.split(" ");
+      return { operation: instruction[0], argument: Number(instruction[1]) };
+    });
 
   return {
     inputToPrint: currentInput, // *optional - inputToPrint will be printed if available
