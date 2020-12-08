45e643689c2ea625466fe5f7947c9816b2802418
Matej Capkovic <mcapkovic@gmail.com>
2020-12-08 13:47:39 +0100

    Add d8p2

--- src/days/day8/solution.js
+++ src/days/day8/solution.js
@@ -31,11 +31,53 @@ export function partOneCode(input) {
   } while (true);
 }
 
+function runProgram(input) {
+  const usedInstructions = new Set();
+  let prevPosition = 0;
+  let position = 0;
+  let value = 0;
+
+  do {
+    if (usedInstructions.has(position)) return { value, correct: false };
+    if (position === input.length && prevPosition === position - 1)
+      return { value, correct: true };
+    if (position > input.length - 1) return { value, correct: false };
+
+    usedInstructions.add(position);
+
+    const instruction = input[position];
+    const { newValue, positionIncrement } = operations[instruction.operation](
+      instruction.argument,
+      position,
+      value
+    );
+
+    value = newValue;
+    prevPosition = position;
+    position = positionIncrement;
+  } while (true);
+}
 export function partTwoCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part2 answer.";
+  const nopIndex = [];
+  const jumpIndex = [];
+
+  input.forEach((row, index) => {
+    if (row.operation === "nop") nopIndex.push(index);
+    if (row.operation === "jmp") jumpIndex.push(index);
+  });
+
+  do {
+    const newInput = [...input];
+    if (nopIndex.length > 0) {
+      const index = nopIndex.pop();
+      newInput.splice(index, 1, { ...newInput[index], operation: "jmp" });
+    } else if (jumpIndex.length > 0) {
+      const index = jumpIndex.pop();
+      newInput.splice(index, 1, { ...newInput[index], operation: "nop" });
+    }
+    const { value, correct } = runProgram(newInput);
+    if (correct) return value;
+  } while (true);
 }
 
 const e1p1 = `
@@ -50,9 +92,23 @@ jmp -4
 acc +6
 `;
 
+const e1p2 = `
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+nop -4
+acc +6
+`;
+
 export function inputParse(originalInput) {
   let currentInput = originalInput;
   // currentInput = e1p1;
+  // currentInput = e1p2;
+
   let parsedInput = currentInput
     .trim()
     .split("\n")
