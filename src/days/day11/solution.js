9460a0fd46cd33a10656d8aa6accafe22c8fb4a5
Matej Capkovic <mcapkovic@gmail.com>
2020-12-11 20:26:53 +0100

    Refactor d11p1

--- src/days/day11/solution.js
+++ src/days/day11/solution.js
@@ -1,78 +1,48 @@
-const neighborhood = {
-  0: (input, row, col) => {
-    const seat = input[row - 1] && input[row - 1][col - 1];
-    if (seat) return seat;
-    return "no-seat";
-  },
-  1: (input, row, col) => {
-    const seat = input[row - 1] && input[row - 1][col];
-    if (seat) return seat;
-    return "no-seat";
-  },
-  2: (input, row, col) => {
-    const seat = input[row - 1] && input[row - 1][col + 1];
-    if (seat) return seat;
-    return "no-seat";
-  },
-  3: (input, row, col) => {
-    const seat = input[row][col - 1];
-    if (seat) return seat;
-    return "no-seat";
-  },
-  4: (input, row, col) => {
-    const seat = input[row][col + 1];
-    if (seat) return seat;
-    return "no-seat";
-  },
-  5: (input, row, col) => {
-    const seat = input[row + 1] && input[row + 1][col - 1];
-    if (seat) return seat;
-    return "no-seat";
-  },
+/**
+ * == PART1 ==
+ * check adjacent seat
+ *
+ * == PART2 ==
+ * recursive seat check
+ */
 
-  6: (input, row, col) => {
-    const seat = input[row + 1] && input[row + 1][col];
-    if (seat) return seat;
-    return "no-seat";
-  },
-  7: (input, row, col) => {
-    const seat = input[row + 1] && input[row + 1][col + 1];
-    if (seat) return seat;
-    return "no-seat";
-  },
-};
+const directions = [
+  [-1, -1], // top left
+  [-1, 0], // top
+  [-1, 1], // top right
+  [0, -1], // left
+  [0, 1], // right
+  [1, -1], // bottom left
+  [1, 0], //bottom
+  [1, 1], // bottom right
+];
+
+function getNeighborSeat(input, row, col) {
+  const seat = input[row] && input[row][col];
+  if (seat) return seat;
+  return "no-seat";
+}
 
 function newSeatState(input, row, col, currentState) {
   if (currentState === ".") return currentState;
 
-  // console.log("neighborhood", neighborhood[0](input, row, col));
+  const occupiedCount = directions
+    .map((direction) =>
+      getNeighborSeat(input, row + direction[0], col + direction[1])
+    )
+    .filter((seat) => seat === "#");
 
-  const neighborhoodSeats = [];
-  for (let index = 0; index < 8; index++) {
-    neighborhoodSeats.push(neighborhood[index](input, row, col));
-  }
-
-  const occupiedCount = neighborhoodSeats.filter((seat) => seat === "#");
-
-  // console.log(occupiedCount);
   if (currentState === "L") return occupiedCount.length === 0 ? "#" : "L";
   if (currentState === "#") return occupiedCount.length >= 4 ? "L" : "#";
 }
 
 function runRound(input, loop) {
   if (loop === 0) return input;
-  const newSeating = input.map((row, rowIndex) => {
-    let test = "";
-    for (let colIndex = 0; colIndex < row.length; colIndex++) {
-      const element = row[colIndex];
-      // console.log(element)
-      const newSeat = newSeatState(input, rowIndex, colIndex, element);
-      test = test + newSeat;
-    }
-    return test;
-  });
-  // console.log('loop', loop);
-  // console.log(newSeating);
+  const newSeating = input.map((row, rowIndex) =>
+    [...row]
+      .map((seat, colIndex) => newSeatState(input, rowIndex, colIndex, seat))
+      .join("")
+  );
   return runRound(newSeating, loop - 1);
 }
 
@@ -191,7 +161,7 @@ function runRound2(input, loop) {
 }
 
 export function partTwoCode(input) {
-  const seating = runRound2(input, 300);
+  const seating = runRound2(input, 100);
   console.log(seating);
   const countOccupied = seating
     .map((row) => {
