5596c73e113b0cd438549ab378eb3711702d4f66
Matej Capkovic <mcapkovic@gmail.com>
2020-12-12 07:04:44 +0100

    Add day12p1

--- src/days/day11/solution.js
+++ src/days/day11/solution.js
@@ -7,14 +7,14 @@
  */
 
 const directions = [
-  [-1, -1], // top left
+  [-1, -1],// top left
   [-1, 0], // top
   [-1, 1], // top right
   [0, -1], // left
   [0, 1], // right
   [1, -1], // bottom left
   [1, 0], //bottom
-  [1, 1], // bottom right
+  [1, 1],// bottom right
 ];
 
 function getNeighborSeat(input, row, col) {
--- src/days/day12/solution.js
+++ src/days/day12/solution.js
@@ -1,8 +1,64 @@
+//  N
+//  S
+//  E
+//  W
+//  L
+//  R
+//  F
+
+const directions = ["N", "E", "S", "W"];
+
+const actions = {
+  N: (boat, value) => {
+    const newPosition = [boat.position[0], boat.position[1] + value];
+    return { ...boat, position: newPosition };
+  },
+  S: (boat, value) => {
+    const newPosition = [boat.position[0], boat.position[1] - value];
+    return { ...boat, position: newPosition };
+  },
+  E: (boat, value) => {
+    const newPosition = [boat.position[0] + value, boat.position[1]];
+    return { ...boat, position: newPosition };
+  },
+  W: (boat, value) => {
+    const newPosition = [boat.position[0] - value, boat.position[1]];
+    return { ...boat, position: newPosition };
+  },
+  L: (boat, value) => {
+    const { position, facing } = boat;
+    const turn = value / 90;
+    const directionIndex = directions.indexOf(facing);
+    const newDirection =
+      directionIndex - turn < 0
+        ? directions[directions.length + (directionIndex - turn)]
+        : directions[directionIndex - turn];
+    return { ...boat, facing: newDirection };
+  },
+  R: (boat, value) => {
+    const { position, facing } = boat;
+    const turn = value / 90;
+    const directionIndex = directions.indexOf(facing);
+    const newDirection =
+      directionIndex + turn > 3
+        ? directions[(directionIndex + turn) % directions.length]
+        : directions[directionIndex + turn];
+    return { ...boat, facing: newDirection };
+  },
+  F: (boat, value) => actions[boat.facing](boat, value),
+};
+
 export function partOneCode(input) {
-  /**
-   * space for the code
-   */
-  return "Part1 answer.";
+  let boat = {
+    position: [0, 0],
+    facing: "E",
+  };
+
+  input.forEach((instruction) => {
+    boat = actions[instruction.action](boat, instruction.value)
+  });
+
+  return Math.abs(boat.position[0]) + Math.abs(boat.position[1]);
 }
 
 export function partTwoCode(input) {
@@ -12,12 +68,26 @@ export function partTwoCode(input) {
   return "Part2 answer.";
 }
 
+const e1p1 = `
+F10
+N3
+F7
+R90
+F11
+`;
+
 export function inputParse(originalInput) {
-  const currentInput = originalInput;
-  let parsedInput = currentInput;
-   /**
-   * Add input logic here
-   */
+  let currentInput = originalInput;
+  // currentInput = e1p1;
+  let parsedInput = currentInput
+    .trim()
+    .split("\n")
+    .map((row) => {
+      return {
+        value: Number(row.slice(1)),
+        action: row[0],
+      };
+    });
 
   return {
     inputToPrint: currentInput, // *optional - inputToPrint will be printed if available
